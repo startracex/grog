32f52cc4250e9f6ec6754f273d1064bfc2d66529
STARTRACEX <startraceebr@foxmail.com>
2023-11-18 19:08:03 +0800

    Add wrapper func

--- common.go
+++ common.go
@@ -21,6 +21,20 @@ func Redirect(request Request, response Response, url string, code int) {
 	http.Redirect(response.Writer, request.OriginalRequest, url, code)
 }
 
+// WrapHandlerFunc wrap http.HandlerFunc to HandlerFunc
+func WrapHandlerFunc(hf http.HandlerFunc) HandlerFunc {
+	return func(request Request, response Response) {
+		hf(response.Writer, request.OriginalRequest)
+	}
+}
+
+// WrapHandler wrap http.Handler to HandlerFunc
+func WrapHandler(h http.Handler) HandlerFunc {
+	return func(request Request, response Response) {
+		h.ServeHTTP(response.Writer, request.OriginalRequest)
+	}
+}
+
 // ServeFile call http.ServeFile
 func ServeFile(req Request, res Response, path string) {
 	http.ServeFile(res.Writer, req.OriginalRequest, path)
