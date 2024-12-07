cb209c3b36f9e9303dc710b6cac25e2ba250b7c9
STARTRACEX <startraceebr@foxmail.com>
2024-12-07 13:04:55 +0800

    Remove ddos.go

--- common.go
+++ common.go
@@ -12,10 +12,6 @@ func Upgrade(req Request, res Response) *toolkit.WS {
 	return toolkit.Upgrade(res.Writer, req.OriginalRequest)
 }
 
-func Do(d *http.Request) (*http.Response, error) {
-	return toolkit.Do(d)
-}
-
 // Redirect call http.Redirect
 func Redirect(request Request, response Response, url string, code int) {
 	http.Redirect(response.Writer, request.OriginalRequest, url, code)
