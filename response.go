68e72126fe86ad7f6787df4ed1f631e49e5de264
STARTRACEX <startraceebr@foxmail.com>
2025-01-03 15:02:10 +0800

    Rename receiver of HttpResponse to "r"

--- response.go
+++ response.go
@@ -23,112 +23,112 @@ func NewResponse(res http.ResponseWriter) HttpResponse {
 
 // With write header and call fn
 // It helps you fill in the header and execute the callback fn
-func (res HttpResponse) With(code int, fn func()) {
-	res.WriteHeader(code)
+func (r HttpResponse) With(code int, fn func()) {
+	r.WriteHeader(code)
 	fn()
 }
 
 // StatusText send code http.StatusText(code)
-func (res Response) StatusText(code int) {
-	res.WriteHeader(code)
-	res.String("%d %s", code, http.StatusText(code)+".")
+func (r Response) StatusText(code int) {
+	r.WriteHeader(code)
+	r.String("%d %s", code, http.StatusText(code)+".")
 }
 
 // Status is alias of WriteHeader
-func (res HttpResponse) Status(code int) HttpResponse {
-	return res.WriteHeader(code)
+func (r HttpResponse) Status(code int) HttpResponse {
+	return r.WriteHeader(code)
 }
 
 // WriteHeader write status code
-func (res HttpResponse) WriteHeader(code int) HttpResponse {
-	res.Writer.WriteHeader(code)
-	return res
+func (r HttpResponse) WriteHeader(code int) HttpResponse {
+	r.Writer.WriteHeader(code)
+	return r
 }
 
 /* Write */
 
 // Write call Writer.Write
-func (res HttpResponse) Write(data []byte) (int, error) {
-	return res.Writer.Write(data)
+func (r HttpResponse) Write(data []byte) (int, error) {
+	return r.Writer.Write(data)
 }
 
 // Byte is alias of Write
-func (res HttpResponse) Byte(data []byte) (int, error) {
-	return res.Write(data)
+func (r HttpResponse) Byte(data []byte) (int, error) {
+	return r.Write(data)
 }
 
 // String write string
-func (res HttpResponse) String(format string, a ...any) (int, error) {
+func (r HttpResponse) String(format string, a ...any) (int, error) {
 	if len(a) == 0 {
-		return fmt.Fprint(res.Writer, format)
+		return fmt.Fprint(r.Writer, format)
 	}
-	return fmt.Fprintf(res.Writer, format, a...)
+	return fmt.Fprintf(r.Writer, format, a...)
 }
 
 /* Data encode */
 
 // JSON send JSON encoded data
-func (res HttpResponse) JSON(data any) error {
-	res.ContentType("application/json")
-	encoder := json.NewEncoder(res.Writer)
+func (r HttpResponse) JSON(data any) error {
+	r.ContentType("application/json")
+	encoder := json.NewEncoder(r.Writer)
 	return encoder.Encode(data)
 }
 
 // HTML send HTML template
-func (res HttpResponse) HTML(name string, data any) error {
-	res.ContentType("text/html")
-	return res.Engine.Template.ExecuteTemplate(res.Writer, name, data)
+func (r HttpResponse) HTML(name string, data any) error {
+	r.ContentType("text/html")
+	return r.Engine.Template.ExecuteTemplate(r.Writer, name, data)
 }
 
 /* Cookies */
 
 // SetCookie set a cookie
-func (res HttpResponse) SetCookie(cookie *http.Cookie) HttpResponse {
-	http.SetCookie(res.Writer, cookie)
-	return res
+func (r HttpResponse) SetCookie(cookie *http.Cookie) HttpResponse {
+	http.SetCookie(r.Writer, cookie)
+	return r
 }
 
 /* Headers */
 
 // Header get header
-func (res HttpResponse) Header() http.Header {
-	return res.Writer.Header()
+func (r HttpResponse) Header() http.Header {
+	return r.Writer.Header()
 }
 
 // SetHeader set a header
-func (res HttpResponse) SetHeader(key, value string) HttpResponse {
-	res.Header().Set(key, value)
-	return res
+func (r HttpResponse) SetHeader(key, value string) HttpResponse {
+	r.Header().Set(key, value)
+	return r
 }
 
 // AddHeader add a header
-func (res HttpResponse) AddHeader(key, value string) HttpResponse {
-	res.Header().Add(key, value)
-	return res
+func (r HttpResponse) AddHeader(key, value string) HttpResponse {
+	r.Header().Add(key, value)
+	return r
 }
 
 // DeleteHeader delete a header
-func (res HttpResponse) DeleteHeader(key string) HttpResponse {
-	res.Header().Del(key)
-	return res
+func (r HttpResponse) DeleteHeader(key string) HttpResponse {
+	r.Header().Del(key)
+	return r
 }
 
 // Authorization set header "Authorization"
-func (res HttpResponse) Authorization(scheme, parameters string) {
-	res.SetHeader("Authorization", scheme+" "+parameters)
+func (r HttpResponse) Authorization(scheme, parameters string) {
+	r.SetHeader("Authorization", scheme+" "+parameters)
 }
 
 // BasicAuthorization set header "Authorization" with Basic scheme
-func (res HttpResponse) BasicAuthorization(parameters string) {
-	res.Authorization("Basic", parameters)
+func (r HttpResponse) BasicAuthorization(parameters string) {
+	r.Authorization("Basic", parameters)
 }
 
 // BearerAuthorization set header "Authorization" with Bearer scheme
-func (res HttpResponse) BearerAuthorization(parameters string) {
-	res.Authorization("Bearer", parameters)
+func (r HttpResponse) BearerAuthorization(parameters string) {
+	r.Authorization("Bearer", parameters)
 }
 
 // ContentType set header "Content-Type"
-func (res HttpResponse) ContentType(value string) {
-	res.SetHeader("Content-Type", value)
+func (r HttpResponse) ContentType(value string) {
+	r.SetHeader("Content-Type", value)
 }
