7ab2c78cee66a012a9ee64adf7e68e32258855c9
STARTRACEX <startraceebr@foxmail.com>
2025-06-02 04:22:09 +0800

    refactor: move ServeFile and ServeContent

--- wrapper.go
+++ wrapper.go
@@ -1,7 +1,10 @@
 package grog
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/startracex/grog/websocket"
 )
@@ -24,4 +27,23 @@ func WrapHandler(h http.Handler) HandlerFunc {
 	return func(c Context) {
 		h.ServeHTTP(c.Writer(), c.Request())
 	}
+}
+
+// ServeFile is similar to http.ServeFile, but it won't redirect.
+func ServeFile(c Context, filePath string) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return
+	}
+	ServeContent(c, fi.Name(), fi.ModTime(), f)
+}
+
+// ServeContent serve file content.
+func ServeContent(c Context, name string, modtime time.Time, f io.ReadSeeker) {
+	http.ServeContent(c.Writer(), c.Request(), name, modtime, f)
 }
