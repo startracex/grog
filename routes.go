35686aee7080bcf02faeb8fdc4649c4d29bc8341
STARTRACEX <startraceebr@foxmail.com>
2025-06-14 15:25:44 +0800

    refactor(routes): move RoutesGroup definition to routes.go

--- routes.go
+++ routes.go
@@ -36,3 +36,30 @@ var ErrNoMethod = errors.New("grog: no method")
 func (r *Routes[T]) Search(path string) *router.Router[map[string][]T] {
 	return r.Root.Search(path)
 }
+
+
+type RoutesGroup[T any] struct {
+	Prefix      string
+	Middlewares []T
+	Engine      *Engine[T]
+}
+
+func (group *RoutesGroup[T]) Group(prefix string, middlewares ...T) *RoutesGroup[T] {
+	engine := group.Engine
+	newGroup := &RoutesGroup[T]{
+		Prefix:      group.Prefix + prefix,
+		Engine:      engine,
+		Middlewares: middlewares,
+	}
+	newGroup.Middlewares = append(newGroup.Middlewares, middlewares...)
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RoutesGroup[T]) AddRoute(method string, pattern string, handlers []T) {
+	group.Engine.Routes.AddRoute(method, group.Prefix+pattern, handlers)
+}
+
+func (group *RoutesGroup[T]) Use(middlewares ...T) {
+	group.Middlewares = append(group.Middlewares, middlewares...)
+}
