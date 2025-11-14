8f49fc674855a5a0f52be404382414084cfe1090
Shiro Wang <startraceebr@foxmail.com>
2025-11-14 08:51:12 +0800

    fix(middleware): update traceback depth and formatting

--- middleware.go
+++ middleware.go
@@ -58,13 +58,13 @@ func AutoOptions() HandlerFunc {
 
 func trace(message string) string {
 	var pcs [32]uintptr
-	n := runtime.Callers(4, pcs[:])
+	n := runtime.Callers(0, pcs[:])
 	var str strings.Builder
 	str.WriteString(message + "\nTraceback:")
 	for _, pc := range pcs[:n] {
 		fn := runtime.FuncForPC(pc)
 		file, line := fn.FileLine(pc)
-		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+		str.WriteString(fmt.Sprintf("\n  %s:%d", file, line))
 	}
 	return str.String()
 }
